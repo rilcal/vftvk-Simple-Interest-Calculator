a0c7a2a6c424adf765735af748cc55d6e77312f6
Riley <rileycallaghan96@gmail.com>
2021-04-21 16:18:58 -0400

    Fix SIR calculation
    Fixes the issue that the rate of return was not a percentage

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function compute() {
   r = document.getElementById("rate").value;
   p = document.getElementById("principal").value;
   y = document.getElementById("years").value;
-  result = p * r * y;
+  result = p * r * y * 0.01;
   console.log(result);
   toShow = `If you deposit ${p}\nat an interest rate of ${r}%\nYou will revieve an amount of ${result}\nin the year ${
     2021 + Number(y)
