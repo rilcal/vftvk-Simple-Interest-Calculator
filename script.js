262e433fc50d1169edd8b892de76f4660593fe49
Riley <rileycallaghan96@gmail.com>
2021-04-21 16:14:01 -0400

    Fix Calulator
    Fixes the calculator app to reflect desired appearence and function.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  r = document.getElementById("rate").value;
+  p = document.getElementById("principal").value;
+  y = document.getElementById("years").value;
+  result = p * r * y;
+  console.log(result);
+  toShow = `If you deposit ${p}\nat an interest rate of ${r}%\nYou will revieve an amount of ${result}\nin the year ${
+    2021 + Number(y)
+  }`;
+  document.getElementById("result").innerText = toShow;
 }
-        
+
+function updateRate() {
+  r = document.getElementById("rate").value;
+  console.log(r);
+  document.getElementById("rate-result").innerText = r + "%";
+}
