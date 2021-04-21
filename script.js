42a7f484c7c53a934b266e6f00df80d8e81f6a18
Riley <rileycallaghan96@gmail.com>
2021-04-21 16:34:27 -0400

    Adjust App to match Assignment
    Tweaked app to fit assignment

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 function compute() {
   r = document.getElementById("rate").value;
   p = document.getElementById("principal").value;
+  if (p < 0) {
+    alert("Principal must be a positive number.");
+    document.getElementById("principal").focus();
+    return;
+  } else if (p == 0) {
+    alert("Principal must be a positive number.");
+    document.getElementById("principal").focus();
+    return;
+  }
   y = document.getElementById("years").value;
   result = p * r * y * 0.01;
   console.log(result);
-  toShow = `If you deposit ${p}\nat an interest rate of ${r}%\nYou will revieve an amount of ${result}\nin the year ${
+  toShow = `If you deposit <span style="background-color: yellow;">${p}</span><br />at an interest rate of <span style="background-color: yellow;">${r}</span>%<br />You will revieve an amount of <span style="background-color: yellow;">${result}</span> <br />in the year <span style="background-color: yellow;">${
     2021 + Number(y)
-  }`;
-  document.getElementById("result").innerText = toShow;
+  }</span>`;
+  document.getElementById("result").innerHTML = toShow;
 }
 
 function updateRate() {
